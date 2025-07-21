10925cb06548eb4555435d227bb431623917ba48
Your Name <you@example.com>
2025-07-21 17:39:37 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/chrono-timeline/sw.js', { scope: '/chrono-timeline/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
